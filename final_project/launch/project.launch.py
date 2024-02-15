fe147bf04d6237efe1f0b162b2ccb73b1d9ba257
dylan-asmar <asmar@stanford.edu>
2024-02-14 18:25:29 -0800

    changed to use moveit launch

--- final_project/launch/project.launch.py
+++ final_project/launch/project.launch.py
@@ -9,20 +9,24 @@ from ament_index_python import get_package_share_directory
 from launch_ros.actions import Node
 
 # Declare constanct for sim file path
-SIM_FILE_PATH = '~/interbotix_ws/src/interbotix_ros_rovers/interbotix_ros_xslocobots/interbotix_xslocobot_sim/launch'
+# SIM_FILE_PATH = '~/interbotix_ws/src/interbotix_ros_rovers/interbotix_ros_xslocobots/interbotix_xslocobot_sim/launch'
+SIM_FILE_PATH = '~/interbotix_ws/src/interbotix_ros_rovers/interbotix_ros_xslocobots/interbotix_xslocobot_moveit/launch'
 
 def generate_launch_description():
     robot_model_arg = DeclareLaunchArgument('robot_model', default_value='locobot_wx250s')
     use_lidar_arg = DeclareLaunchArgument('use_lidar', default_value='true')
+    hardware_type_arg = DeclareLaunchArgument('hardware_type', default_value='gz_classic')
 
     return LaunchDescription([
         robot_model_arg,
         use_lidar_arg,
+        hardware_type_arg,
         IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(os.path.expanduser(f'{SIM_FILE_PATH}/xslocobot_gz_classic.launch.py')),
+            PythonLaunchDescriptionSource(os.path.expanduser(f'{SIM_FILE_PATH}/xslocobot_moveit.launch.py')),
             launch_arguments={
                 'robot_model': LaunchConfiguration('robot_model'),
-                'use_lidar': LaunchConfiguration('use_lidar')
+                'use_lidar': LaunchConfiguration('use_lidar'),
+                'hardware_type': LaunchConfiguration('hardware_type'),
             }.items()
         ),
         TimerAction(period=5.0, actions=[
