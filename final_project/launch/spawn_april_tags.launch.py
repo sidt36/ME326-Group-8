6ae6501099c40ff4441cad8f1e2b53f06ea7410e
dylan-asmar <asmar@stanford.edu>
2024-02-29 13:56:35 -0800

    updated simulation layout based on classrom trial setup after class 2/29

--- final_project/launch/spawn_april_tags.launch.py
+++ final_project/launch/spawn_april_tags.launch.py
@@ -16,7 +16,7 @@ def generate_launch_description():
                 '-entity', 'apriltag_0', 
                 '-file', f'{models_path}/Apriltag36_11_00000/model.sdf',
                 '-x', '0.0', 
-                '-y', '1.0', 
+                '-y', '0.75',
                 '-z', '0.0',
                 '-R', '0.0',
                 '-P', '0.0',
@@ -30,7 +30,7 @@ def generate_launch_description():
                 '-entity', 'apriltag_1', 
                 '-file', f'{models_path}/Apriltag36_11_00001/model.sdf',
                 '-x', '0.0', 
-                '-y', '-1.0', 
+                '-y', '-0.75', 
                 '-z', '0.0',
                 '-R', '0.0',
                 '-P', '0.0',
@@ -43,8 +43,8 @@ def generate_launch_description():
             arguments=[
                 '-entity', 'apriltag_2', 
                 '-file', f'{models_path}/Apriltag36_11_00002/model.sdf',
-                '-x', '1.25', 
-                '-y', '0.0', 
+                '-x', '1.75', 
+                '-y', '0.75', 
                 '-z', '0.0',
                 '-R', '0.0',
                 '-P', '0.0',
@@ -57,8 +57,22 @@ def generate_launch_description():
             arguments=[
                 '-entity', 'apriltag_3', 
                 '-file', f'{models_path}/Apriltag36_11_00003/model.sdf',
-                '-x', '2.5', 
-                '-y', '1.0', 
+                '-x', '1.75', 
+                '-y', '-0.75', 
+                '-z', '0.0',
+                '-R', '0.0',
+                '-P', '0.0',
+                '-Y', '3.1416'],
+            output='screen'
+        ),
+        Node(
+            package='gazebo_ros',
+            executable='spawn_entity.py',
+            arguments=[
+                '-entity', 'apriltag_4', 
+                '-file', f'{models_path}/Apriltag36_11_00004/model.sdf',
+                '-x', '0.75', 
+                '-y', '0.5', 
                 '-z', '0.0',
                 '-R', '0.0',
                 '-P', '0.0',
@@ -69,14 +83,42 @@ def generate_launch_description():
             package='gazebo_ros',
             executable='spawn_entity.py',
             arguments=[
-                '-entity', 'apriltag_4', 
-                '-file', f'{models_path}/Apriltag36_11_00004/model.sdf',
-                '-x', '2.5', 
-                '-y', '-1.0', 
+                '-entity', 'apriltag_5', 
+                '-file', f'{models_path}/Apriltag36_11_00005/model.sdf',
+                '-x', '1.0', 
+                '-y', '0.5', 
                 '-z', '0.0',
                 '-R', '0.0',
                 '-P', '0.0',
-                '-Y', '3.1416'],
+                '-Y', '0.0'],
             output='screen'
-        )
+        ),
+        Node(
+            package='gazebo_ros',
+            executable='spawn_entity.py',
+            arguments=[
+                '-entity', 'apriltag_6', 
+                '-file', f'{models_path}/Apriltag36_11_00006/model.sdf',
+                '-x', '0.75', 
+                '-y', '-0.5', 
+                '-z', '0.0',
+                '-R', '0.0',
+                '-P', '0.0',
+                '-Y', '0.0'],
+            output='screen'
+        ),
+        Node(
+            package='gazebo_ros',
+            executable='spawn_entity.py',
+            arguments=[
+                '-entity', 'apriltag_7', 
+                '-file', f'{models_path}/Apriltag36_11_00007/model.sdf',
+                '-x', '1.0', 
+                '-y', '-0.5', 
+                '-z', '0.0',
+                '-R', '0.0',
+                '-P', '0.0',
+                '-Y', '0.0'],
+            output='screen'
+        ),
     ])
